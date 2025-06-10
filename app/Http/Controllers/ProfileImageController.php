637e926d3817d92346ce72ff792c80c8cbdd6595
roshanmomand <1028086@hr.nl>
2025-06-10 14:45:47 +0200

    Models,Controllers, Resources, Factories and Seeders added

---
+++ app/Http/Controllers/ProfileImageController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreProfileImageRequest;
+use App\Http\Requests\UpdateProfileImageRequest;
+use App\Models\ProfileImage;
+
+class ProfileImageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreProfileImageRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ProfileImage $profileImage)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ProfileImage $profileImage)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateProfileImageRequest $request, ProfileImage $profileImage)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ProfileImage $profileImage)
+    {
+        //
+    }
+}
---
+++ database/migrations/2025_06_10_115917_create_fruits_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('fruits', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->decimal('price',8,2)->default(0);
+            $table->string('big_img_file_path')->nullable();
+            $table->string('small_img_file_path')->nullable();
+            $table->decimal('weight',8,2);
+            $table->string('serving_size')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('fruits');
+    }
+};
