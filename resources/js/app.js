b9eb3cc8c9a5a1cd9078e5c60a0bbaf500627c3f
Thijs Kraaijenbos <47988622+ThijsKraaijenbos@users.noreply.github.com>
2025-06-11 22:34:10 +0200

    Add copy feature to token output

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,20 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+
+let copyIconDiv = document.getElementById("copy-icon-div")
+
+const clickHandler = () => {
+    let copyIconPath = document.getElementById("copy-icon-path")
+    let copyText = document.getElementById("outputToken")
+    copyText.select();
+    navigator.clipboard.writeText(copyText.value);
+
+    copyIconPath.style.fill = "#4ade80"
+    setTimeout(() =>
+        copyIconPath.style.fill = "#2c2e30", 1000
+    )
+
+}
+copyIconDiv.addEventListener("click", () => clickHandler())
